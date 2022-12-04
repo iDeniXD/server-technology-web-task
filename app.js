966a6ae070c03b0bd87c3794e66589456189e320
denis <denis.grabovsky@gmail.com>
2022-12-04 19:07:42 +0200

    cors fix 1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieparser())
 app.use(cors({origin:true,credentials: true}));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
     res.setHeader('Access-Control-Allow-Credentials', true);
